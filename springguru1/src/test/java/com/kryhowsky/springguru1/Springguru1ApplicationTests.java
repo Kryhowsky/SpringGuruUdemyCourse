1ae1c285769ac4619b64911cf6e3cd51e6da80c6
P Kry <piter71-1997@go2.pl>
2021-10-01 14:45:11 +0200

    Initial commit

---
+++ springguru1/src/test/java/com/kryhowsky/springguru1/Springguru1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kryhowsky.springguru1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springguru1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
